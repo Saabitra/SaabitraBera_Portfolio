258f60696c6940a1bf5115e501a06133bfda0bbb
Saabitra Bera <saabitrabera@gmail.com>
2025-01-12 18:28:02 +0530

    Projects commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,10 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
+import disastertracker from "../assets/projects/disaster-tracker.jpg";
+import saabitraportfolio from "../assets/projects/saabitra-portfolio.jpg";
+import weatheraireporter from "../assets/projects/weather-ai-reporter.jpg";
+
 export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
@@ -40,33 +44,33 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
-    image: project1,
+    title: "Disaster Tracker",
+    image: disastertracker,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A fully functioning disaster tracker website that tracks ongoing disasters, displays affected locations, and provides detailed statistics on property damage and casualties.",
+    technologies: ["HTML", "CSS", "JavaScript", "Google Maps API", "NASA EONET API"],
   },
   {
-    title: "Task Management App",
-    image: project2,
+    title: "Weather AI Reporter",
+    image: weatheraireporter,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "A fully functioning AI weather website that provides detailed weather reports, including temperature, UV index, sunrise and sunset times, with graphical and summarized forecasts.",
+    technologies: ["NextJS", "TailwindCSS", "Tremor", "Open-Meteo API", "OpenAI API"],
   },
   {
     title: "Portfolio Website",
-    image: project3,
+    image: saabitraportfolio,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["React", "TailwindCSS", "Framer Motion"],
   },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
+  // {
+  //   title: "Blogging Platform",
+  //   image: project4,
+  //   description:
+  //     "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+  //   technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  // },
 ];
 
 export const CONTACT = {
