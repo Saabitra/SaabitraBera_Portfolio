3291962f54fefc6969f35d6099736e2f55da112b
Saabitra Bera <saabitrabera@gmail.com>
2025-01-12 21:15:30 +0530

    Navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { FaInstagram } from "react-icons/fa6";
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
     <div className="flex flex-shrink-0 items-center">
-        <a href="#"><img className="mx-2 w-10" src={logo} alt="logo" /></a>
+        <a href="https://saabitra-bera-portfolio.vercel.app/"><img className="mx-2 w-10" src={logo} alt="logo" /></a>
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <a href="https://www.linkedin.com/in/saabitra-bera-47124b154/"><FaLinkedin /></a>
